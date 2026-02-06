3a5c85145431d5ab82bff874a4a749fc45eb39fe
Alexandre <alexandre.remaud44@gmail.com>
2026-02-06 22:20:07 +0100

    remove backend

--- backend-old/src/middlewares/notFound.middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-
-export const notFound = (req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({
-    message: `Route ${req.originalUrl} not found`,
-  });
-};
