ca5a9be182c6a91959f716af2bf1fecdfb762040
Alexandre <alexandre.remaud44@gmail.com>
2026-02-10 14:32:31 +0100

    fix issue with vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
